0542b24d00943cb34e5fb040ab8a203edf3caee6
granico <orrganik@gmail.com>
2014-02-12 11:33:47 +0100

    Rails

---
+++ baloo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baloo::Application.config.secret_token = '223940bb7a24309e0b30e7d09d6119d4f022dc94a52e27532781eea8e9c0306c553d2f2f4207c4e9a535d1dedc0a9b659f57cb2af3d074728f1e323786773a09'
